e117522efce4e685c933aeeb3c52a950b53657c9
DanielePriuli <priulidaniele@gmail.com>
2025-04-21 15:31:26 +0200

    Add files via upload
    BookPals is a book-themed social where the main entities are Users, Books, Authors and, obviously me, the admin. Here you should be able to follow the authors you like and get a notification whenever a new book is loaded in the database by the admin or the authors themselves. Users could choose to become "BookPals" and share their readings and their opinions about a book.
    In this branch, BookPals is developed without the aid of frameworks (only HTML, CSS, JS and BOOTSTRAP are used) and there aren't any realistic data to see, only the view is showed. If you wanna see more realistic features, switch to other branches with frameworks or another repository where both front end and back end are developed.

---
+++ BookPals/static/js/general_fx.js
@@ -0,0 +1,35 @@
+var menuButtons=document.getElementsByClassName("nav-option");
+var hambMenu=document.getElementById("menu-butt");
+var nav=document.getElementsByTagName("nav")[0];
+var advSrcMenu=document.getElementById("adv-search-div");
+var userIcon=document.getElementById("user-butt");
+var loginForm=document.getElementById("login-form-div");
+var pswShow=document.querySelectorAll("#login-form label")[1].childNodes[1];
+var pswShow=document.querySelectorAll("#login-form label")[1].childNodes[3];
+undefined
+var pswInput=document.querySelectorAll("#login-form label")[1].childNodes[1];
+undefined
+function showPsw() {
+    if (pswInput.type=="password") {
+        pswInput.type="text";
+    } else {
+        pswInput.type="password";
+    }
+    pswShow.classList.toggle("bi-eye");
+}
+function showNav() {
+    advSrcMenu.classList.remove("show");
+    nav.classList.toggle("show");
+}
+function showAdvSrc() {
+    nav.classList.remove("show");
+    advSrcMenu.classList.toggle("show");
+}
+function showLoginForm() {
+    loginForm.classList.toggle("show");
+}
+hambMenu.addEventListener("click", showNav);
+document.getElementById("adv-search").addEventListener("click", showAdvSrc);
+userIcon.addEventListener("click", showLoginForm);
+pswShow.addEventListener("click", showPsw);
+
