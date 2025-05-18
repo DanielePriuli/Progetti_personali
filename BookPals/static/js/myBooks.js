f4f9a9b0177fa8da157dab43010331c4ae2cc876
DanielePriuli <priulidaniele@gmail.com>
2025-05-18 11:41:52 +0200

    Add files via upload
    custom carousel animations both working (prev slide and next slide). The custom carousel is the last one.

--- BookPals/static/js/myBooks.js
+++ BookPals/static/js/myBooks.js
@@ -9,7 +9,7 @@ function nextSlide() {
            inner.classList.add("left-inner");
            setTimeout(() => {
                 inner.classList.remove("c-activated");
-           }, 1500);
+           }, 1000);
            if (i==carouselInners.length-1) {
                 carouselInners[0].classList.add("c-activated");
                 carouselInners[1].classList.remove("left-inner");
@@ -22,7 +22,37 @@ function nextSlide() {
     }
 }
 function prevSlide() {
-
+         for (let i = 0; i < carouselInners.length; i++) {
+        const inner = carouselInners[i];
+        if (inner.classList.length==3 && inner.classList[2]=="c-activated") {
+               inner.classList.add("right-inner");
+               setTimeout(() => {
+               inner.classList.remove("left-inner")
+               inner.classList.remove("c-activated");
+               inner.classList.remove("right-inner");
+               }, 1000);
+           if (i==0) {
+               carouselInners[2].classList.remove("left-inner");
+               carouselInners[2].classList.add("c-activated");
+               carouselInners[1].classList.remove("right-inner");
+               carouselInners[1].classList.add("hide");
+               carouselInners[1].classList.add("left-inner");
+               setTimeout(() => {
+                    carouselInners[1].classList.remove("hide");
+               }, 1000);
+           } else {
+               carouselInners[i-1].classList.remove("left-inner");
+               carouselInners[i-1].classList.add("c-activated");
+               carouselInners[(i==2)? i-2:2].classList.remove("right-inner");
+               carouselInners[(i==2)? i-2:2].classList.add("hide");
+               carouselInners[(i==2)? i-2:2].classList.add("left-inner");
+               setTimeout(() => {
+                    carouselInners[(i==2)? i-2:2].classList.remove("hide");
+               }, 1000);
+           }
+           break;
+        }
+    }
 }
 function animate() {
     document.getElementsByClassName("c-carousel-inner")[0].classList.toggle("animate");
